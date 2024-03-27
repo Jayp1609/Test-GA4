73f5f90066cce31229426ae4ad88a558753f8d96
Jayp1609 <96374272+Jayp1609@users.noreply.github.com>
2024-03-27 17:27:22 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,10 +1,19 @@
+"use client";
 import Link from "next/link";
 import styles from "./page.module.css";
+import { sendGTMEvent } from "@next/third-parties/google";
 
 export default function Home() {
   return (
     <main className={styles.main}>
       <div>Fetch Data</div>
+      <button
+        onClick={() => {
+          sendGTMEvent({ event: "buttonClicked", value: "xyz" });
+        }}
+      >
+        Click Here for google event
+      </button>
       <Link href="productlist">Go to productlist</Link>
       <Link href="productserver">Go to productlist server</Link>
     </main>

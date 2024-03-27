dfebf0ee3f9332da3f07ca62296aad8bdc975ce3
Jayp1609 <96374272+Jayp1609@users.noreply.github.com>
2024-03-27 17:50:24 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@
 import { GoogleTagManager } from "@next/third-parties/google";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Head from "next/head";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -10,16 +11,24 @@ export default function RootLayout({ children }) {
     <html lang="en">
       <head>
         <link rel="stylesheet" href="/style.css" />
-      
-<script async src="https://www.googletagmanager.com/gtag/js?id=G-7HS8SEPRD4"></script>
-<script>
-  window.dataLayer = window.dataLayer || [];
-  function gtag(){dataLayer.push(arguments)}
-  gtag('js', new Date());
-
-  gtag('config', 'G-7HS8SEPRD4');
-</script>
       </head>
+      <Head>
+        {/* Google tag (gtag.js) */}
+        <script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-7HS8SEPRD4"
+        ></script>
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-7HS8SEPRD4');
+        `,
+          }}
+        ></script>
+      </Head>
       <GoogleTagManager gtmId="GTM-NDQHW6GD" />
       <body className={inter.className}>{children}</body>
     </html>

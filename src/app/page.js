2e425e3baee40f20239a7b04f5fdc53a9cc8bf2c
Jayp1609 <96374272+Jayp1609@users.noreply.github.com>
2024-03-28 13:04:15 +0530

    Update page.js

--- src/app/page.js
+++ src/app/page.js
@@ -1,16 +1,25 @@
 "use client";
+import React from "react";
 import Link from "next/link";
 import styles from "./page.module.css";
-import { sendGTMEvent } from "@next/third-parties/google";
+//import { sendGTMEvent } from "@next/third-parties/google";
+import useGoogleAnalytics from "../../services/useGoogleAnalytics";
 
 export default function Home() {
+  const pushEventToDataLayer = useGoogleAnalytics("G-339K42BX2L");
+
+  const handleClick = () => {
+    pushEventToDataLayer({ event: "login" });
+  };
+
   return (
     <main className={styles.main}>
       <div>Fetch Data</div>
       <button
-        onClick={() => {
-          sendGTMEvent({ event: "buttonClicked", value: "xyz" });
-        }}
+        // onClick={() => {
+        //   sendGTMEvent({ event: "buttonClicked", value: "xyz" });
+        // }}
+        onClick={handleClick}
       >
         Click Here for google event
       </button>

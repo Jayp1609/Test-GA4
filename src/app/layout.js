bc55ca3716040fc3903c1ab1fb8aceee92f99fdf
bhargav <jaypatel160902@gmail.com>
2024-04-12 13:01:02 +0530

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,10 +2,6 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <head>
-        <meta
-          httpEquiv="Content-Security-Policy"
-          content="script-src 'self' https://testing-plugin.onrender.com"
-        />
         <link rel="stylesheet" href="/style.css" />
         {/* <script src="https://testing-plugin.onrender.com/api/comments-script" /> */}
         <script

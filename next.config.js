d26b03f9b9a9c9b728f5788c7dd0af7b043ef2a0
bhargav <jaypatel160902@gmail.com>
2024-04-12 11:06:39 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -32,5 +32,36 @@ const nextConfig = {
   },
   // output: "export",
 };
-
-module.exports = nextConfig;
+module.exports = {
+  ...nextConfig,
+  async headers() {
+    return [
+      {
+        source: "/(.*)?",
+        headers: [
+          {
+            key: "X-Frame-Options",
+            value: "SAMEORIGIN",
+          },
+          {
+            key: "X-Content-Type-Options",
+            value: "nosniff",
+          },
+          {
+            key: "Referrer-Policy",
+            value: "strict-origin-when-cross-origin",
+          },
+          {
+            key: "Permissions-Policy",
+            value:
+              'geolocation=(self "https://test-ga-4.vercel.app"), camera=()',
+          },
+          {
+            key: "Strict-Transport-Security",
+            value: "max-age=31536000; includeSubDomains; preload",
+          },
+        ],
+      },
+    ];
+  },
+};
